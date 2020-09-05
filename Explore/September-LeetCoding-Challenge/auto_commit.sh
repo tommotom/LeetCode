3f9982600f8cb005499d57455de9e58b44e7f31f
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2020-09-05 18:53:23 +0900

    auto commit

---
+++ Explore/September-LeetCoding-Challenge/auto_commit.sh
@@ -0,0 +1,8 @@
+path="$HOME/IdeaProjects/LeetCode/Explore/September-LeetCoding-Challenge/"
+clear
+cd $path
+echo ""
+echo $path
+git add .
+git commit -m 'auto commit'
+git push -u origin master
---
+++ Explore/September-LeetCoding-Challenge/create.sh
@@ -0,0 +1,8 @@
+path="$HOME/IdeaProjects/LeetCode/Explore/September-LeetCoding-Challenge/"
+clear
+cd $path
+echo -n INPUT_DATE:
+read str
+mkdir $str
+cd $str
+touch Solution.py
