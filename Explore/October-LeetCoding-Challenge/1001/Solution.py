c499d82e4ed5a8a09773bbb0aa2935c074fc8a5f
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2020-10-01 16:25:40 +0900

    auto commit

---
+++ Explore/October-LeetCoding-Challenge/1001/Solution.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+class RecentCounter:
+
+    def __init__(self):
+        self.history = deque()
+
+    def ping(self, t: int) -> int:
+        self.history.append(t)
+
+        while self.history[0] < t - 3000:
+            self.history.popleft()
+
+        return len(self.history)
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
---
+++ Explore/October-LeetCoding-Challenge/create.sh
@@ -0,0 +1,8 @@
+path="$HOME/IdeaProjects/LeetCode/Explore/October-LeetCoding-Challenge/"
+clear
+cd $path
+echo -n "INPUT_DATE(MMDD):"
+read str
+mkdir $str
+cd $str
+touch Solution.py
