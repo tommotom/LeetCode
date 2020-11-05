682da1fe0eca22b92bc722a22d3b1518e27c7c65
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2020-11-05 17:06:24 +0900

    auto commit

---
+++ Explore/November-LeetCoding-Challenge/1105/Solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        even = 0
+        for p in position:
+            if p % 2 == 0: even += 1
+        return min(even, len(position)-even)
