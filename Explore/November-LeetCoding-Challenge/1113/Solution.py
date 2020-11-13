552391d8cf984ef4202c9bf52bd5c403cc6e5886
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2020-11-13 19:41:33 +0900

    auto commit

---
+++ Explore/November-LeetCoding-Challenge/1113/Solution.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root: return
+
+        q = deque([(0, root)])
+
+        while q:
+            level, node = q.popleft()
+            if q and q[0][0] == level:
+                node.next = q[0][1]
+            if not node.left: continue
+            q.append((level+1, node.left))
+            q.append((level+1, node.right))
+
+        return root
