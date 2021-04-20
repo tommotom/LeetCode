4ef3d5feef74ad6d5784743085b468f48d25896f
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2021-04-20 16:31:03 +0900

    auto commit

---
+++ Explore/April-LeetCoding-Challenge/0420/Solution.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root: return []
+
+        stack = [root]
+        ans = []
+        while stack:
+            node = stack.pop()
+            ans.append(node.val)
+            for child in node.children[::-1]:
+                stack.append(child)
+        return ans
