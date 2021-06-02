abf21cbdffd4e2681b19fb56c6de770395fc2cf9
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2021-06-02 16:23:21 +0900

    auto commit

---
+++ Explore/June-LeetCoding-Challenge/0602/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    @lru_cache(None)
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if not s1 and not s2 and not s3: return True
+        ret = []
+        if s1 and s3 and s1[0] == s3[0]:
+            ret.append(self.isInterleave(s1[1:], s2, s3[1:]))
+        if s2 and s3 and s2[0] == s3[0]:
+            ret.append(self.isInterleave(s1, s2[1:], s3[1:]))
+        return any(ret)
