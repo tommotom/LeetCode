1f52793821a36ab71006b0900c4872fac41782ef
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2021-06-23 23:24:09 +0900

    auto commit

--- Explore/June-LeetCoding-Challenge/0623/Solution.py
+++ Explore/June-LeetCoding-Challenge/0623/Solution.py
@@ -1,42 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def minFlips(self, mat: List[List[int]]) -> int:
-        def isValid(mat):
-            for m in mat:
-                if sum(m) != 0: return False
-            return True
-
-        def flipAt(i, j, mat):
-            nonlocal rows, cols
-            mat[i][j] = 1 - mat[i][j]
-            if i > 0: mat[i-1][j] = 1 - mat[i-1][j]
-            if j > 0: mat[i][j-1] = 1 - mat[i][j-1]
-            if i+1 < rows: mat[i+1][j] = 1 - mat[i+1][j]
-            if j+1 < cols: mat[i][j+1] = 1 - mat[i][j+1]
-            return mat
-
-        def hash(mat):
-            nonlocal rows, cols
-            n = 0
-            ret = 0
-            for i in range(rows):
-                for j in range(cols):
-                    ret += mat[i][j] * (10 ** n)
-                    n += 1
-            return ret
-
-        rows, cols = len(mat), len(mat[0])
-
-        q = deque([(0, mat)])
-        visited = set([hash(mat)])
-
-        while q:
-            c, arr = q.popleft()
-            if isValid(arr): return c
-            for i in range(rows):
-                for j in range(cols):
-                    tmp = flipAt(i, j, copy.deepcopy(arr))
-                    s = hash(tmp)
-                    if not s in visited:
-                        q.append([c+1, tmp])
-                        visited.add(s)
-        return -1
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        dummy = ListNode(None)
+        dummy.next = head
+        node = dummy
+        for _ in range(left-1):
+            node = node.next
+        rev_head = rev_tail = node.next
+        for _ in range(right-left):
+            tmp = rev_tail.next
+            rev_tail.next = tmp.next
+            node.next = tmp
+            tmp.next = rev_head
+            rev_head = tmp
+        return dummy.next
