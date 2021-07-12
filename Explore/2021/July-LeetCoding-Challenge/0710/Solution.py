979876c68815b56a5b63e3bf055291416cfc0007
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2021-07-12 19:16:06 +0900

    auto commit

--- Explore/2021/July-LeetCoding-Challenge/0710/Solution.py
+++ Explore/2021/July-LeetCoding-Challenge/0710/Solution.py
@@ -1,24 +1,27 @@
-class MedianFinder:
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        mod = 10 ** 9 + 7
+        dp = [0 for _ in range(len(s)+1)]
+        dp[-1] = 1
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == "*":
+                dp[i] += 9 * dp[i+1]
+            elif s[i] != "0":
+                dp[i] += dp[i+1]
 
-    def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.heaps = [], []
+            if i == len(s) - 1: continue
 
-    def addNum(self, num: int) -> None:
-        small, large = self.heaps
-        heapq.heappush(small, -heapq.heappushpop(large, num))
-        if len(small) > len(large):
-            heapq.heappush(large, -heapq.heappop(small))
+            if s[i] == "1" or s[i] == "*":
+                if s[i+1] == "*":
+                    dp[i] += 9 * dp[i+2]
+                else:
+                    dp[i] += dp[i+2]
 
-    def findMedian(self) -> float:
-        small, large = self.heaps
-        if len(small) < len(large):
-            return float(large[0])
-        return (large[0] - small[0]) / 2
+            if s[i] == "2" or s[i] == "*":
+                if s[i+1] == "*":
+                    dp[i] += 6 * dp[i+2]
+                elif s[i+1] < "7":
+                    dp[i] += dp[i+2]
 
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+            dp[i] %= mod
+        return dp[0]
---
+++ Explore/2021/July-LeetCoding-Challenge/0712/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_to_t, t_to_s = {}, {}
+        for i in range(len(s)):
+            if s[i] in s_to_t:
+                if s_to_t[s[i]] != t[i]:
+                    return False
+            else:
+                s_to_t[s[i]] = t[i]
+
+            if t[i] in t_to_s:
+                if t_to_s[t[i]] != s[i]:
+                    return False
+            else:
+                t_to_s[t[i]] = s[i]
+
+        return True
