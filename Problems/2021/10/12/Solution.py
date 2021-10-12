8bbd561a1174e07010dfdda56ff7bffe5be2f031
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2021-10-12 22:47:45 +0900

    auto commit

---
+++ Problems/2021/10/12/Solution.py
@@ -0,0 +1,17 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l, r = 1, n
+        while l <= r:
+            m = (l + r) // 2
+            val = guess(m)
+            if val == -1:
+                r = m-1
+            elif val == 1:
+                l = m+1
+            else:
+                return m
