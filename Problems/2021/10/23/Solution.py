9a600d1a112dba9bfb3e1ea7924063fab516d929
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2021-10-24 11:22:44 +0900

    auto commit

---
+++ Problems/2021/10/23/Solution.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+
+        max_depth = 0
+        node = root
+        while node:
+            max_depth += 1
+            node = node.left
+
+        def has_n_nodes_at_last_row(root, n):
+            depth = 1
+            node = root
+            took = 0
+            while depth < max_depth:
+                if took + pow(2, max_depth-1-depth) >= n:
+                    node = node.left
+                else:
+                    node = node.right
+                    took += pow(2, max_depth-1-depth)
+                depth += 1
+            return node is not None
+
+        l, r = 1, pow(2, max_depth-1)+1
+        while l < r:
+            m = l + (r - l) // 2
+            if has_n_nodes_at_last_row(root, m):
+                l = m + 1
+            else:
+                r = m
+
+        return pow(2, max_depth-1) + l-2
