c234ba2c3f904e82c78ad31248c2384a9088adbf
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2021-11-18 17:15:43 +0900

    auto commit

---
+++ Problems/2021/11/18/Solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            idx = abs(nums[i]) - 1
+            if nums[idx] > 0:
+                nums[idx] *= -1
+        return [i+1 for i in range(len(nums)) if nums[i] > 0]
