da4254cbf65d80e91df4f3327664a1b3fec0c4e0
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2022-01-18 12:53:30 +0900

    auto commit

---
+++ Problems/2022/01/18/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n == 0: return True
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 1: continue
+            if i > 0 and flowerbed[i-1] == 1: continue
+            if i+1 < len(flowerbed) and flowerbed[i+1] == 1: continue
+            flowerbed[i] = 1
+            n -= 1
+            if n == 0: return True
+
+        return False
