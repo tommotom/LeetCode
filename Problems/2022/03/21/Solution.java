ca74ba61dfb75880b6936158aecd2a5204de5415
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2022-03-21 12:12:06 +0900

    auto commit

--- Problems/2022/03/21/Solution.java
+++ Problems/2022/03/21/Solution.java
@@ -1,32 +1,19 @@
 class Solution {
   public List<Integer> partitionLabels(String s) {
-    Map<String, Integer> counter1 = new HashMap<>();
-    for (String ch: s.split("")) {
-      counter1.put(ch, counter1.getOrDefault(ch, 0) + 1);
+    int[] last = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+      last[s.charAt(i) - 'a'] = i;
     }
 
-    List<Integer> ans = new ArrayList<>();
-    Map<String, Integer> counter2 = new HashMap<>();
-    for (String ch: s.split("")) {
-      counter1.put(ch, counter1.get(ch)-1);
-      counter2.put(ch, counter2.getOrDefault(ch, 0) + 1);
-      if (isValidPartition(counter1, counter2)) {
-        ans.add(counter2.values().stream().mapToInt(Integer::intValue).sum());
-        counter2 = new HashMap<>();
+    int j = 0, anchor = 0;
+    List<Integer> ans = new ArrayList();
+    for (int i = 0; i < s.length(); i++) {
+      j = Math.max(j, last[s.charAt(i) - 'a']);
+      if (i == j) {
+        ans.add(i - anchor + 1);
+        anchor = i + 1;
       }
-    }
-    if (counter2.size() > 0) {
-      ans.add(counter2.values().stream().mapToInt(Integer::intValue).sum());
     }
     return ans;
-  }
-
-  private boolean isValidPartition(Map<String, Integer> c1, Map<String, Integer> c2) {
-    for (String key: c2.keySet()) {
-      if (c1.getOrDefault(key, 0) > 0) {
-        return false;
-      }
-    }
-    return true;
   }
 }
