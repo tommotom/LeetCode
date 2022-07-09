4491278476782a4e599edaa5dd89e78a5567d8a2
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2022-07-09 20:03:55 +0900

    auto commit

---
+++ Problems/2022/07/09/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int maxResult(int[] nums, int k) {
+    LinkedList<Integer> list = new LinkedList<>();
+    list.add(0);
+    for (int i = 1; i < nums.length; i++) {
+      if (list.getFirst() < i-k) {
+        list.removeFirst();
+      }
+      nums[i] += nums[list.getFirst()];
+      while (list.size() > 0 && nums[list.getLast()] <= nums[i]) {
+        list.removeLast();
+      }
+      list.addLast(i);
+    }
+    return nums[nums.length-1];
+  }
+}
