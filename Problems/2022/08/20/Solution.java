9134ae3c5d5bf527fb5df64226475bdd8ce86403
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2022-08-20 11:28:34 +0900

    auto commit

---
+++ Problems/2022/08/20/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        int n = stations.length;
+        long[] dp = new long[n+1];
+        dp[0] = startFuel;
+        for (int i = 0; i < n; i++) {
+            for (int t = i; t >= 0; t--) {
+                if (dp[t] >= stations[i][0]) {
+                    dp[t+1] = Math.max(dp[t+1], dp[t] + (long) stations[i][1]);
+                }
+            }
+        }
+        for (int i = 0; i < n+1; i++) {
+            if (dp[i] >= target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
