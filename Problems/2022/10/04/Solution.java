9c2baba3a76f9942df0509e10d9854335b2eed4a
tommotom <tomohiro19911129@gmail.com>
2022-10-04 10:49:24 +0900

    auto commit

---
+++ Problems/2022/10/04/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root != null && root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        if (root.left == null) {
+            return hasPathSum(root.right, targetSum - root.val);
+        }
+        if (root.right == null) {
+            return hasPathSum(root.left, targetSum - root.val);
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
