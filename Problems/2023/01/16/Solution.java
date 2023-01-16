818c26ce8b2f5db9820b46c4fc21d2705f2e238f
tommotom <tomohiro19911129@gmail.com>
2023-01-16 14:01:27 +0900

    auto commit

---
+++ Problems/2023/01/16/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> ans = new ArrayList<>();
+        int n = intervals.length, i = 0;
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            ans.add(intervals[i++]);
+        }
+        while (i < n && !(intervals[i][1] < newInterval[0] || newInterval[1] < intervals[i][0])) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        ans.add(newInterval);
+        while (i < n) {
+            ans.add(intervals[i++]);
+        }
+        return ans.toArray(new int[ans.size()][2]);
+    }
+}
