4093dfd49c65501dd2a1503a6dfc6e277ddc3864
tommotom <tomohiro19911129@gmail.com>
2023-03-06 21:33:34 +0900

    auto commit

---
+++ Problems/2023/03/06/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int l = 0, r = arr.length;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            int missing = arr[m] - m - 1;
+            if (missing < k) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return l + k;
+    }
+}
