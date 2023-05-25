1531b136cd77d048af9f0965b3be9628fed72cdc
tommotom <tomohiro19911129@gmail.com>
2023-05-25 11:52:25 +0900

    auto commit

---
+++ Problems/2023/05/25/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double new21Game(int n, int k, int maxPts) {
+        if (k == 0 || n >= k + maxPts) {return 1.0;}
+
+        double sum = 1.0;
+        double p = 0;
+
+        double[] dp = new double[n+1];
+        dp[0] = 1.0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = sum / maxPts;
+
+            if (i < k) {
+                sum += dp[i];
+            } else {
+                p += dp[i];
+            }
+
+            if (i >= maxPts) {
+                sum -= dp[i-maxPts];
+            }
+        }
+
+        return p;
+    }
+}
