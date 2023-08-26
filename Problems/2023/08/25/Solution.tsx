e0ea7fbc8f987964529806405758088782b7df13
tommotom <tomohiro19911129@gmail.com>
2023-08-26 22:15:20 +0900

    auto commit

--- Problems/2023/08/25/Solution.tsx
+++ Problems/2023/08/25/Solution.tsx
@@ -2,32 +2,24 @@ function isInterleave(s1: string, s2: string, s3: string): boolean {
     if (s1.length + s2.length !== s3.length) {
         return false;
     }
-    const memo = new Map();
-    function helper(s1: string, s2: string, s3: string): boolean {
-        if (s1.length === 0) {
-            return s2 === s3;
-        }
-        if (s2.length === 0) {
-            return s1 === s3;
-        }
-
-        const key = s1 + " " + s2;
-
-        if (memo.has(key)) {
-            return memo.get(key);
-        }
-
-        if (s1.substring(0, 1) == s3.substring(0, 1) && isInterleave(s1.substring(1), s2, s3.substring(1))) {
-            memo.set(key, true);
+    const tried = new Set();
+    function helper(i: number, j: number) {
+        if (i === s1.length && j === s2.length) {
             return true;
         }
-
-        if (s2.substring(0, 1) == s3.substring(0, 1) && isInterleave(s1, s2.substring(1), s3.substring(1))) {
-            memo.set(key, true);
+        const key = i.toString() + " " + j.toString();
+        if (tried.has(key)) {
+            return false;
+        }
+        if (i < s1.length && s1.charAt(i) === s3.charAt(i+j) && helper(i+1, j)) {
             return true;
         }
+        if (j < s2.length && s2.charAt(j) === s3.charAt(i+j) && helper(i, j+1)) {
+            return true;
+        }
+        tried.add(key);
         return false;
     }
 
-    return helper(s1, s2, s3);
+    return helper(0, 0);
 };
