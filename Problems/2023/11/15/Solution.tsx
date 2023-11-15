69742213ef71ed6977412a238fec897c1dc22984
Tomohiro Yamakawa <tomohiro19911129@gmail.com>
2023-11-15 20:43:48 +0900

    auto commit

---
+++ Problems/2023/11/15/Solution.tsx
@@ -0,0 +1,8 @@
+function maximumElementAfterDecrementingAndRearranging(arr: number[]): number {
+    arr.sort((a, b) => a - b);
+    arr[0] = 1;
+    for (let i = 1; i < arr.length; i++) {
+        arr[i] = Math.min(arr[i], arr[i-1] + 1);
+    }
+    return arr[arr.length-1];
+};
