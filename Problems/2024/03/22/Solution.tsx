925b4fa58dee52ba4ad808bba9ab7531f2a61450
tommotom <tomohiro19911129@gmail.com>
2024-03-22 10:27:27 +0900

    auto commit

---
+++ Problems/2024/03/22/Solution.tsx
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function isPalindrome(head: ListNode | null): boolean {
+    const arr = [];
+    while (head !== null) {
+        arr.push(head.val);
+        head = head.next;
+    }
+    let l = 0, r = arr.length-1;
+    while (l < r) {
+        if (arr[l++] !== arr[r--]) {
+            return false;
+        }
+    }
+    return true;
+};
