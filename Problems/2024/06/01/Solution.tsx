13b72ec425cc52c78900eac39833ba32bd053466
tommotom <tomohiro19911129@gmail.com>
2024-06-01 10:04:49 +0900

    auto commit

---
+++ Problems/2024/06/01/Solution.tsx
@@ -0,0 +1,7 @@
+function scoreOfString(s: string): number {
+    let ans = 0;
+    for (let i = 0; i < s.length-1; i++) {
+        ans += Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1));
+    }
+    return ans;
+};
