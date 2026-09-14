55b8f505bfbcfd66afcae0829a18278c9fe8759f
tommotom <tomohiro19911129@gmail.com>
2026-09-14 18:46:57 +0900

    auto commit

---
+++ Problems/2026/09/14/Solution.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn is_rectangle_overlap(rec1: Vec<i32>, rec2: Vec<i32>) -> bool {
+        if rec1[0] == rec2[0] && rec1[1] == rec2[1] && rec1[2] == rec2[2] && rec1[3] == rec2[3] {
+            return true;
+        }
+        fn overlap1(rec1: &Vec<i32>, rec2: &Vec<i32>) -> bool {
+            (rec1[0] < rec2[0] && rec2[0] < rec1[2]) || (rec1[0] < rec2[2] && rec2[2] < rec1[2]) || (rec2[0] < rec1[0] && rec1[0] < rec2[2]) || (rec2[0] < rec1[2] && rec1[2] < rec2[2])
+
+        }
+        fn overlap2(rec1: &Vec<i32>, rec2: &Vec<i32>) -> bool {
+            (rec1[1] < rec2[1] && rec2[1] < rec1[3]) || (rec1[1] < rec2[3] && rec2[3] < rec1[3]) || (rec2[1] < rec1[1] && rec1[1] < rec2[3]) || (rec2[1] < rec1[3] && rec1[3] < rec2[3])
+
+        }
+        overlap1(&rec1, &rec2) && overlap2(&rec1, &rec2)
+    }
+}
